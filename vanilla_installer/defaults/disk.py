9fedadbb5c0fb987785c63b7395c7e778f2efd39
Mirko Brombin <send@mirko.pm>
2022-10-30 16:27:52 +0100

    implement partitioning

--- vanilla_installer/defaults/disk.py
+++ vanilla_installer/defaults/disk.py
@@ -21,25 +21,123 @@ from gi.repository import Gtk, Gio, GLib, Adw
 from vanilla_installer.core.disks import DisksManager
 
 
-@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/dialog-disk.ui')
-class VanillaDefaultDiskPartModal(Adw.Window):
-    __gtype_name__ = 'VanillaDefaultDiskPartModal'
-
-    def __init__(self, window, **kwargs):
-        super().__init__(**kwargs)
-        self.__window = window
-        self.set_transient_for(self.__window)
-
-
 @Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/widget-disk.ui')
 class VanillaDefaultDiskEntry(Adw.ActionRow):
     __gtype_name__ = 'VanillaDefaultDiskEntry'
 
-    def __init__(self, disk, **kwargs):
+    chk_button = Gtk.Template.Child()
+
+    def __init__(self, disk, chk_group, use_radio, **kwargs):
         super().__init__(**kwargs)
         self.__disk = disk
         self.set_title(disk.name)
         self.set_subtitle(disk.pretty_size)
+        self.chk_button.set_group(chk_group)
+
+        if not use_radio:
+            # since there is only one disk and for some reason GtkCheckButton
+            # only works as a GtkRadioButton only when there are more than one
+            # we create a fake GtkRadioButton to check the first one
+            fake_chk_button = Gtk.CheckButton()
+            fake_chk_button.set_group(self.chk_button)
+            self.chk_button.set_active(True)
+            self.chk_button.set_sensitive(False)
+            self.chk_button.set_tooltip_text(_("This is the only disk available and cannot be deselected!"))
+    
+    @property
+    def is_active(self):
+        if self.chk_button.get_active():
+            return True
+
+    @property
+    def disk(self):
+        return self.__disk
+
+
+@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/widget-partition.ui')
+class VanillaDefaultPartitionEntry(Adw.ExpanderRow):
+    __gtype_name__ = 'VanillaDefaultPartitionEntry'
+
+    combo_fs = Gtk.Template.Child()
+    str_list_fs = Gtk.Template.Child()
+    combo_mp = Gtk.Template.Child()
+    str_list_mp = Gtk.Template.Child()
+
+    def __init__(self, partition, **kwargs):
+        super().__init__(**kwargs)
+        self.__partition = partition
+        self.__name = partition.partition
+        self.set_title(partition.partition)
+        self.set_subtitle(partition.pretty_size)
+    
+    @property
+    def selected_fs(self):
+        index = self.combo_fs.get_selected()
+        return self.str_list_fs.get_string(index)
+
+    @property
+    def selected_mountpoint(self):
+        index = self.combo_mp.get_selected()
+        return self.str_list_mp.get_string(index)
+
+    @property
+    def name(self):
+        return self.__name
+
+
+@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/dialog-disk.ui')
+class VanillaDefaultDiskPartModal(Adw.Window):
+    __gtype_name__ = 'VanillaDefaultDiskPartModal'
+
+    chk_entire_disk = Gtk.Template.Child()
+    chk_manual_part = Gtk.Template.Child()
+    group_partitions = Gtk.Template.Child()
+    btn_cancel = Gtk.Template.Child()
+    btn_apply = Gtk.Template.Child()
+
+    def __init__(self, window, parent, disk, **kwargs):
+        super().__init__(**kwargs)
+        self.__window = window
+        self.__parent = parent
+        self.__disk = disk
+        self.set_transient_for(self.__window)
+        self.chk_entire_disk.set_group(self.chk_manual_part)
+        self.__registry_partitions = []
+
+        # signals
+        self.chk_manual_part.connect('toggled', self.__on_chk_manual_part_toggled)
+        self.btn_cancel.connect('clicked', self.__on_btn_cancel_clicked)
+        self.btn_apply.connect('clicked', self.__on_btn_apply_clicked)
+
+        for partition in self.__disk.partitions:
+            entry = VanillaDefaultPartitionEntry(partition)
+            self.group_partitions.add(entry)
+            self.__registry_partitions.append(entry)
+
+    def __on_chk_manual_part_toggled(self, widget):
+        self.group_partitions.set_visible(widget.get_active())
+
+    def __on_btn_cancel_clicked(self, widget):
+        self.destroy()
+
+    def __on_btn_apply_clicked(self, widget):
+        self.__parent.set_partition_recipe(self.partition_recipe)
+        self.destroy()
+
+    @property
+    def partition_recipe(self):
+        # returns a dictionary with all the partitions with the choosen
+        # filesystem and mountpoint
+        recipe = {}
+        if self.chk_entire_disk.get_active():
+            return {"auto": self.__disk.name}
+        
+        for partition in self.__registry_partitions:
+            recipe[partition.name] = {
+                "fs": partition.selected_fs,
+                "mp": partition.selected_mountpoint
+            }
+        return recipe
 
 
 @Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/default-disk.ui')
@@ -56,12 +154,21 @@ class VanillaDefaultDisk(Adw.Bin):
         self.__distro_info = distro_info
         self.__key = key
         self.__step = step
+        self.__registry_disks = []
         self.__disks = DisksManager()
+        self.__partition_recipe = None
 
         # append the disks widgets
-        for disk in self.__disks.all_disks:
-            entry = VanillaDefaultDiskEntry(disk)
+        chk_group = None
+        count_disks = len(self.__disks.all_disks)
+        for index, disk in enumerate(self.__disks.all_disks):
+            entry = VanillaDefaultDiskEntry(disk, chk_group, use_radio=count_disks > 1)
             self.group_disks.add(entry)
+
+            if index == 0:
+                chk_group = entry.chk_button
+
+            self.__registry_disks.append(entry)
 
         # signals
         self.btn_next.connect("clicked", self.__window.next)
@@ -71,5 +178,15 @@ class VanillaDefaultDisk(Adw.Bin):
         return {}
     
     def __on_configure_clicked(self, button):
-        modal = VanillaDefaultDiskPartModal(self.__window)
-        modal.present()
+        for entry in self.__registry_disks:
+            if not entry.is_active:
+                continue
+
+            modal = VanillaDefaultDiskPartModal(self.__window, self, entry.disk)
+            modal.present()
+            break
+
+    def set_partition_recipe(self, recipe):
+        self.__partition_recipe = recipe
+        from pprint import pprint
+        pprint(recipe)
