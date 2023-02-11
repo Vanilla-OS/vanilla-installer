539f4a6330a159ec7bab0d73a225c1c19f296bba
Mateus Melchiades <mateusbmelchiades@gmail.com>
2023-02-11 13:39:50 -0300

    Code style improvements

--- vanilla_installer/defaults/disk.py
+++ vanilla_installer/defaults/disk.py
@@ -23,9 +23,9 @@ from typing import Union
 from vanilla_installer.core.disks import DisksManager, Partition
 
 
-@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/widget-disk.ui')
+@Gtk.Template(resource_path="/org/vanillaos/Installer/gtk/widget-disk.ui")
 class VanillaDefaultDiskEntry(Adw.ActionRow):
-    __gtype_name__ = 'VanillaDefaultDiskEntry'
+    __gtype_name__ = "VanillaDefaultDiskEntry"
 
     chk_button = Gtk.Template.Child()
 
@@ -36,7 +36,9 @@ class VanillaDefaultDiskEntry(Adw.ActionRow):
 
         if disk.size < 50000000000:
             self.set_sensitive(False)
-            self.set_subtitle(_("Not enough space: {0}/{1}").format(disk.pretty_size, "50 GB"))
+            self.set_subtitle(
+                _("Not enough space: {0}/{1}").format(disk.pretty_size, "50 GB")
+            )
         else:
             self.set_subtitle(disk.pretty_size)
             self.chk_button.set_group(chk_group)
@@ -49,7 +51,9 @@ class VanillaDefaultDiskEntry(Adw.ActionRow):
                 fake_chk_button.set_group(self.chk_button)
                 self.chk_button.set_active(True)
                 self.chk_button.set_sensitive(False)
-                self.chk_button.set_tooltip_text(_("This is the only disk available and cannot be deselected!"))
+                self.chk_button.set_tooltip_text(
+                    _("This is the only disk available and cannot be deselected!")
+                )
 
     @property
     def is_active(self):
@@ -65,9 +69,9 @@ class VanillaDefaultDiskEntry(Adw.ActionRow):
         return self.__partition.disk_block
 
 
-@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/widget-partition.ui')
+@Gtk.Template(resource_path="/org/vanillaos/Installer/gtk/widget-partition.ui")
 class PartitionSelector(Adw.PreferencesPage):
-    __gtype_name__ = 'PartitionSelector'
+    __gtype_name__ = "PartitionSelector"
 
     boot_part_expand = Gtk.Template.Child()
     efi_part_expand = Gtk.Template.Child()
@@ -116,7 +120,7 @@ class PartitionSelector(Adw.PreferencesPage):
     }
     __valid_root_partitions = False
 
-    __partition_fs_types = [ "btrfs", "ext4", "ext3", "fat32", "xfs" ]
+    __partition_fs_types = ["btrfs", "ext4", "ext3", "fat32", "xfs"]
 
     def __init__(self, parent, partitions, **kwargs):
         super().__init__(**kwargs)
@@ -149,7 +153,7 @@ class PartitionSelector(Adw.PreferencesPage):
             self.swap_part_expand.add_row(widget)
             self.__selected_partitions["swap_part_expand"]["fstype"] = "swap"
 
-    def __generate_partition_list_widgets(self, default_fs = "btrfs"):
+    def __generate_partition_list_widgets(self, default_fs="btrfs"):
         checkbuttons = []
         partition_widgets = []
 
@@ -194,7 +198,9 @@ class PartitionSelector(Adw.PreferencesPage):
             return
 
         for k, val in self.__selected_partitions.items():
-            if val["partition"] == None and (k != "swap_part_expand" or self.use_swap_part.get_active()):
+            if val["partition"] == None and (
+                k != "swap_part_expand" or self.use_swap_part.get_active()
+            ):
                 self.__parent.set_btn_apply_sensitive(False)
                 return
 
@@ -203,16 +209,24 @@ class PartitionSelector(Adw.PreferencesPage):
 
     def __check_root_partitions_size_equal(self):
         if self.__selected_partitions["abroot_a_part_expand"]["partition"]:
-            a_root_part_size = self.__selected_partitions["abroot_a_part_expand"]["partition"].size
+            a_root_part_size = self.__selected_partitions["abroot_a_part_expand"][
+                "partition"
+            ].size
         else:
             a_root_part_size = None
 
         if self.__selected_partitions["abroot_b_part_expand"]["partition"]:
-            b_root_part_size = self.__selected_partitions["abroot_b_part_expand"]["partition"].size
+            b_root_part_size = self.__selected_partitions["abroot_b_part_expand"][
+                "partition"
+            ].size
         else:
             b_root_part_size = None
 
-        if a_root_part_size and b_root_part_size and a_root_part_size != b_root_part_size:
+        if (
+            a_root_part_size
+            and b_root_part_size
+            and a_root_part_size != b_root_part_size
+        ):
             self.abroot_a_part_expand.get_style_context().add_class("error")
             self.abroot_b_part_expand.get_style_context().add_class("error")
             self.root_sizes_differ_error.set_visible(True)
@@ -228,16 +242,29 @@ class PartitionSelector(Adw.PreferencesPage):
     def __on_use_swap_toggled(self, widget, state):
         if state == False:
             children = self.swap_part_expand.get_child().observe_children()
-            child_rows = children.get_item(children.get_n_items() - 1).get_child().observe_children()
+            child_rows = (
+                children.get_item(children.get_n_items() - 1)
+                .get_child()
+                .observe_children()
+            )
 
             for i in range(child_rows.get_n_items()):
-                child_row = child_rows.get_item(i);
-                row_checkbutton = child_row.observe_children().get_item(0).observe_children().get_item(0).observe_children().get_item(0)
+                child_row = child_rows.get_item(i)
+                row_checkbutton = (
+                    child_row.observe_children()
+                    .get_item(0)
+                    .observe_children()
+                    .get_item(0)
+                    .observe_children()
+                    .get_item(0)
+                )
                 row_checkbutton.set_active(False)
 
             self.__selected_partitions["swap_part_expand"]["partition"] = None
             self.swap_part_expand.set_title(_("No partition selected"))
-            self.swap_part_expand.set_subtitle(_("Please select a partition from the options below"))
+            self.swap_part_expand.set_subtitle(
+                _("Please select a partition from the options below")
+            )
             self.__update_partition_rows()
 
         self.__update_apply_button_status()
@@ -249,15 +276,26 @@ class PartitionSelector(Adw.PreferencesPage):
             self.abroot_a_part_expand,
             self.abroot_b_part_expand,
             self.home_part_expand,
-            self.swap_part_expand
+            self.swap_part_expand,
         ]:
             children = row.get_child().observe_children()
-            child_rows = children.get_item(children.get_n_items() - 1).get_child().observe_children()
+            child_rows = (
+                children.get_item(children.get_n_items() - 1)
+                .get_child()
+                .observe_children()
+            )
 
             for i in range(child_rows.get_n_items()):
-                child_row = child_rows.get_item(i);
+                child_row = child_rows.get_item(i)
                 row_partition = child_row.get_title()
-                row_checkbutton = child_row.observe_children().get_item(0).observe_children().get_item(0).observe_children().get_item(0)
+                row_checkbutton = (
+                    child_row.observe_children()
+                    .get_item(0)
+                    .observe_children()
+                    .get_item(0)
+                    .observe_children()
+                    .get_item(0)
+                )
 
                 # The row where partition was selected still has to be sensitive
                 if row_checkbutton.get_active():
@@ -266,14 +304,19 @@ class PartitionSelector(Adw.PreferencesPage):
 
                 is_used = False
                 for _, val in self.__selected_partitions.items():
-                    if val["partition"] is not None and val["partition"].partition == row_partition:
+                    if (
+                        val["partition"] is not None
+                        and val["partition"].partition == row_partition
+                    ):
                         is_used = True
                 child_row.set_sensitive(not is_used)
 
     def __on_check_button_toggled(self, widget):
         action_row = widget.get_parent().get_parent().get_parent()
         children = action_row.get_child().observe_children()
-        dropdown = children.get_item(children.get_n_items() - 1).observe_children().get_item(0)
+        dropdown = (
+            children.get_item(children.get_n_items() - 1).observe_children().get_item(0)
+        )
 
         rows = action_row.get_parent().observe_children()
         for i in range(rows.get_n_items()):
@@ -281,7 +324,11 @@ class PartitionSelector(Adw.PreferencesPage):
             row_children = row.get_child().observe_children()
 
             # Sets all dropdowns as not sensitive
-            row_dropdown = row_children.get_item(row_children.get_n_items() - 1).observe_children().get_item(0)
+            row_dropdown = (
+                row_children.get_item(row_children.get_n_items() - 1)
+                .observe_children()
+                .get_item(0)
+            )
             if row_dropdown:
                 row_dropdown.set_sensitive(False)
 
@@ -299,7 +346,9 @@ class PartitionSelector(Adw.PreferencesPage):
         else:
             expander_row.set_subtitle(f"{size}")
 
-        self.__selected_partitions[expander_row.get_buildable_id()]["partition"] = self.__partition_idx(partition)
+        self.__selected_partitions[expander_row.get_buildable_id()][
+            "partition"
+        ] = self.__partition_idx(partition)
 
         # Sets already selected partitions as not sensitive
         self.__update_partition_rows()
@@ -323,9 +372,9 @@ class PartitionSelector(Adw.PreferencesPage):
         return self.__selected_partitions
 
 
-@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/dialog-disk.ui')
+@Gtk.Template(resource_path="/org/vanillaos/Installer/gtk/dialog-disk.ui")
 class VanillaDefaultDiskPartModal(Adw.Window):
-    __gtype_name__ = 'VanillaDefaultDiskPartModal'
+    __gtype_name__ = "VanillaDefaultDiskPartModal"
     __gsignals__ = {
         "partitioning-set": (GObject.SignalFlags.RUN_FIRST, None, (str,)),
     }
@@ -338,7 +387,6 @@ class VanillaDefaultDiskPartModal(Adw.Window):
     launch_gparted = Gtk.Template.Child()
     manual_part_expand = Gtk.Template.Child()
 
-
     def __init__(self, window, parent, disk, **kwargs):
         super().__init__(**kwargs)
         self.__window = window
@@ -350,10 +398,10 @@ class VanillaDefaultDiskPartModal(Adw.Window):
         self.default_width, self.default_height = self.get_default_size()
 
         # signals
-        self.chk_manual_part.connect('toggled', self.__on_chk_manual_part_toggled)
-        self.chk_entire_disk.connect('toggled', self.__on_chk_entire_disk_toggled)
-        self.btn_cancel.connect('clicked', self.__on_btn_cancel_clicked)
-        self.btn_apply.connect('clicked', self.__on_btn_apply_clicked)
+        self.chk_manual_part.connect("toggled", self.__on_chk_manual_part_toggled)
+        self.chk_entire_disk.connect("toggled", self.__on_chk_entire_disk_toggled)
+        self.btn_cancel.connect("clicked", self.__on_btn_cancel_clicked)
+        self.btn_apply.connect("clicked", self.__on_btn_apply_clicked)
         self.launch_gparted.connect("clicked", self.__on_launch_gparted)
 
         self.__partition_selector = PartitionSelector(self, self.__disk.partitions)
@@ -361,13 +409,13 @@ class VanillaDefaultDiskPartModal(Adw.Window):
 
     def __on_chk_manual_part_toggled(self, widget):
         self.group_partitions.set_visible(widget.get_active())
-        self.set_default_size(self.default_width, 800);
+        self.set_default_size(self.default_width, 800)
         self.manual_part_expand.set_expanded(widget.get_active())
         self.set_btn_apply_sensitive(False)
 
     def __on_chk_entire_disk_toggled(self, widget):
         self.set_btn_apply_sensitive(True)
-        self.set_default_size(self.default_width, self.default_height);
+        self.set_default_size(self.default_width, self.default_height)
 
     def __on_btn_cancel_clicked(self, widget):
         self.destroy()
@@ -378,7 +426,7 @@ class VanillaDefaultDiskPartModal(Adw.Window):
         self.destroy()
 
     def __on_launch_gparted(self, widget):
-        subprocess.Popen(['gparted'])
+        subprocess.Popen(["gparted"])
 
     def set_btn_apply_sensitive(self, val):
         self.btn_apply.set_sensitive(val)
@@ -397,7 +445,9 @@ class VanillaDefaultDiskPartModal(Adw.Window):
 
         recipe["disk"] = self.__disk.disk
         for _, info in self.__partition_selector.selected_partitions.items():
-            if not isinstance(info["partition"], Partition):  # Partition can be None if user didn't configure swap
+            if not isinstance(
+                info["partition"], Partition
+            ):  # Partition can be None if user didn't configure swap
                 continue
             recipe[info["partition"].partition] = {
                 "fs": info["fstype"],
@@ -408,9 +458,9 @@ class VanillaDefaultDiskPartModal(Adw.Window):
         return recipe
 
 
-@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/dialog-disk-confirm.ui')
+@Gtk.Template(resource_path="/org/vanillaos/Installer/gtk/dialog-disk-confirm.ui")
 class VanillaDefaultDiskConfirmModal(Adw.Window):
-    __gtype_name__ = 'VanillaDefaultDiskConfirmModal'
+    __gtype_name__ = "VanillaDefaultDiskConfirmModal"
 
     btn_cancel = Gtk.Template.Child()
     btn_apply = Gtk.Template.Child()
@@ -423,8 +473,8 @@ class VanillaDefaultDiskConfirmModal(Adw.Window):
         self.default_width, self.default_height = self.get_default_size()
 
         # signals
-        self.btn_cancel.connect('clicked', self.__on_btn_cancel_clicked)
-        self.btn_apply.connect('clicked', self.__on_btn_apply_clicked)
+        self.btn_cancel.connect("clicked", self.__on_btn_cancel_clicked)
+        self.btn_apply.connect("clicked", self.__on_btn_apply_clicked)
 
         for partition, values in partition_recipe.items():
             entry = Adw.ActionRow()
@@ -436,10 +486,12 @@ class VanillaDefaultDiskConfirmModal(Adw.Window):
                 if partition == "disk":
                     continue
                 entry.set_title(partition)
-                entry.set_subtitle(_("Will be formatted in {} and mounted in {}").format(
-                    partition_recipe[partition]["fs"],
-                    partition_recipe[partition]["mp"],
-                ))
+                entry.set_subtitle(
+                    _("Will be formatted in {} and mounted in {}").format(
+                        partition_recipe[partition]["fs"],
+                        partition_recipe[partition]["mp"],
+                    )
+                )
             self.group_partitions.add(entry)
 
     def __on_btn_cancel_clicked(self, widget):
@@ -450,9 +502,9 @@ class VanillaDefaultDiskConfirmModal(Adw.Window):
         self.destroy()
 
 
-@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/default-disk.ui')
+@Gtk.Template(resource_path="/org/vanillaos/Installer/gtk/default-disk.ui")
 class VanillaDefaultDisk(Adw.Bin):
-    __gtype_name__ = 'VanillaDefaultDisk'
+    __gtype_name__ = "VanillaDefaultDisk"
 
     btn_next = Gtk.Template.Child()
     btn_configure = Gtk.Template.Child()
@@ -499,7 +551,7 @@ class VanillaDefaultDisk(Adw.Bin):
             entry.disk.update_partitions()
 
             modal = VanillaDefaultDiskPartModal(self.__window, self, entry.disk)
-            modal.connect('partitioning-set', on_modal_close_request)
+            modal.connect("partitioning-set", on_modal_close_request)
             modal.present()
             break
 
@@ -509,4 +561,3 @@ class VanillaDefaultDisk(Adw.Bin):
     def __on_btn_next_clicked(self, button):
         modal = VanillaDefaultDiskConfirmModal(self.__window, self.__partition_recipe)
         modal.present()
-# disk.py
--- vanilla_installer/utils/processor.py
+++ vanilla_installer/utils/processor.py
@@ -29,14 +29,13 @@ logger = logging.getLogger("Installer::Processor")
 
 
 class Processor:
-
     @staticmethod
     def gen_swap_size():
         """
         Reference: https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/storage_administration_guide/ch-swapspace#doc-wrapper
         """
         mem = os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES")
-        mem = mem / (1024.0 ** 3)
+        mem = mem / (1024.0**3)
         if mem <= 2:
             return int(mem * 3 * 1024)
         elif mem > 2 and mem <= 8:
@@ -50,18 +49,22 @@ class Processor:
     def gen_install_script(log_path, pre_run, post_run, finals):
         logger.info("processing the following final data: %s", finals)
 
-        #manifest_remove = "/cdrom/casper/filesystem.manifest-remove"
-        #if not os.path.exists(manifest_remove):
+        # manifest_remove = "/cdrom/casper/filesystem.manifest-remove"
+        # if not os.path.exists(manifest_remove):
         manifest_remove = "/tmp/filesystem.manifest-remove"
         with open(manifest_remove, "w") as f:
             f.write("vanilla-installer\n")
             f.write("gparted\n")
 
         arguments = [
-            "sudo", "distinst",
-            "-s", "'/cdrom/casper/filesystem.squashfs'",
-            "-r", f"'{manifest_remove}'",
-            "-h", "'vanilla'",
+            "sudo",
+            "distinst",
+            "-s",
+            "'/cdrom/casper/filesystem.squashfs'",
+            "-r",
+            f"'{manifest_remove}'",
+            "-h",
+            "'vanilla'",
         ]
 
         is_almost_supported = shutil.which("almost")
@@ -77,9 +80,15 @@ class Processor:
                     arguments = ["echo", f"'{value['password']}'", "|"] + arguments
                     arguments += ["--username", f"'{value['username']}'"]
                     arguments += ["--realname", f"'{value['fullname']}'"]
-                    arguments += ["--profile_icon", "'/usr/share/pixmaps/faces/yellow-rose.jpg'"]
+                    arguments += [
+                        "--profile_icon",
+                        "'/usr/share/pixmaps/faces/yellow-rose.jpg'",
+                    ]
                 elif key == "timezone":
-                    arguments += ["--tz", "'{}/{}'".format(value["region"], value["zone"])]
+                    arguments += [
+                        "--tz",
+                        "'{}/{}'".format(value["region"], value["zone"]),
+                    ]
                     finals_timezone = final
                 elif key == "language":
                     arguments += ["-l", f"'{value}'"]
@@ -90,32 +99,61 @@ class Processor:
                     if "auto" in value:
                         device_block = value["auto"]["disk"]
                         arguments += ["-b", f"'{device_block}'"]
-                        arguments += ["-t", "'{}:gpt'".format(device_block)]
-                        arguments += ["-n", "'{}:primary:start:1024M:fat32:mount=/boot/efi:flags=esp'".format(device_block)]
-                        arguments += ["-n", "'{}:primary:1024M:2048M:ext4:mount=/boot'".format(device_block)]
-                        arguments += ["-n", "'{}:primary:2048M:22528M:btrfs:mount=/'".format(device_block)]
-                        arguments += ["-n", "'{}:primary:22528M:43008M:btrfs:mount=/'".format(device_block)]
-                        arguments += ["-n", "'{}:primary:43008M:end:btrfs:mount=/home'".format(device_block)]
-                        #  arguments += ["-n", "'{}:primary:-{}M:end:swap'".format(value["auto"]["disk"], Processor.gen_swap_size())]
+                        arguments += ["-t", f"'{device_block}:gpt'"]
+                        arguments += [
+                            "-n",
+                            f"'{device_block}:primary:start:1024M:fat32:mount=/boot/efi:flags=esp'",
+                        ]
+                        arguments += [
+                            "-n",
+                            f"'{device_block}:primary:1024M:2048M:ext4:mount=/boot'",
+                        ]
+                        arguments += [
+                            "-n",
+                            f"'{device_block}:primary:2048M:22528M:btrfs:mount=/'",
+                        ]
+                        arguments += [
+                            "-n",
+                            f"'{device_block}:primary:22528M:43008M:btrfs:mount=/'",
+                        ]
+                        arguments += [
+                            "-n",
+                            f"'{device_block}:primary:43008M:end:btrfs:mount=/home'",
+                        ]
                     else:
                         device_block = value["disk"]
                         for partition, values in value.items():
                             if partition == "disk":
                                 arguments += ["-b", f"'{values}'"]
                                 continue
+
+                            partition_number = re.sub(r".*([0-9]+)", r"\1", partition)
                             if values["mp"] == "/boot/efi":
-                                partition_number = re.sub(r".*([0-9]+)", r"\1", partition)
-                                arguments += ["-u", "'{}:{}:{}:mount=/boot/efi:flags=esp'".format(device_block, partition_number, values["fs"])]
+                                arguments += [
+                                    "-u",
+                                    "'{}:{}:{}:mount=/boot/efi:flags=esp'".format(
+                                        device_block, partition_number, values["fs"]
+                                    ),
+                                ]
                             elif values["mp"] == "swap":
-                                partition_number = re.sub(r".*([0-9]+)", r"\1", partition)
-                                arguments += ["-u", "'{}:{}:swap'".format(device_block, partition_number)]
+                                arguments += [
+                                    "-u",
+                                    f"'{device_block}:{partition_number}:swap'",
+                                ]
                             else:
-                                partition_number = re.sub(r".*([0-9]+)", r"\1", partition)
-                                arguments += ["-u", "'{}:{}:{}:mount={}'".format(device_block, partition_number, values["fs"], values["mp"])]
+                                arguments += [
+                                    "-u",
+                                    "'{}:{}:{}:mount={}'".format(
+                                        device_block,
+                                        partition_number,
+                                        values["fs"],
+                                        values["mp"],
+                                    ),
+                                ]
 
         # generating a temporary file to store the distinst command and
         # arguments parsed from the final data
-        with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as f:
             f.write("#!/bin/sh\n")
             f.write("# This file was created by the Vanilla Installer.\n")
             f.write("# Do not edit this file manually!\n\n")
@@ -127,10 +165,12 @@ class Processor:
 
             if "VANILLA_FAKE" in os.environ:
                 logger.info("VANILLA_FAKE is set, skipping the installation process.")
-                f.write("echo 'VANILLA_FAKE is set, skipping the installation process.'\n")
+                f.write(
+                    "echo 'VANILLA_FAKE is set, skipping the installation process.'\n"
+                )
                 f.write("echo 'Printing the configuration instead:'\n")
                 f.write("echo '----------------------------------'\n")
-                f.write('echo "{}"\n'.format(finals))
+                f.write(f'echo "{finals}"\n')
                 f.write("echo '----------------------------------'\n")
                 f.write("sleep 5\n")
                 f.write("exit 1\n")
@@ -142,8 +182,11 @@ class Processor:
             if "VANILLA_SKIP_POSTINSTALL" not in os.environ:
                 f.write("\n")
                 f.write("echo 'Starting the post-installation process ...'\n")
-                f.write("sudo abroot-adapter '{}' '{}'"
-                        .format(json.dumps(finals_disk), json.dumps(finals_timezone)))
+                f.write(
+                    "sudo abroot-adapter '{}' '{}'".format(
+                        json.dumps(finals_disk), json.dumps(finals_timezone)
+                    )
+                )
 
             f.flush()
             f.close()
