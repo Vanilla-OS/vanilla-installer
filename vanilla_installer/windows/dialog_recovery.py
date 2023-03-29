6db85c1b0b9fe4d72bcd8ba44f569a19978f5b0f
mirkobrombin <send@mirko.pm>
2023-03-29 23:33:12 +0200

    implement recovery utils view

---
+++ vanilla_installer/windows/dialog_recovery.py
@@ -0,0 +1,51 @@
+# dialog_recovery.py
+#
+# Copyright 2022 mirkobrombin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundationat version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import webbrowser
+from gi.repository import Gtk, GLib, Adw
+from gettext import gettext as _
+
+
+@Gtk.Template(resource_path='/org/vanillaos/Installer/gtk/dialog-recovery.ui')
+class VanillaRecoveryDialog(Adw.Window):
+    __gtype_name__ = 'VanillaRecoveryDialog'
+
+    row_console = Gtk.Template.Child()
+    row_gparted = Gtk.Template.Child()
+    row_handbook = Gtk.Template.Child()
+    row_web = Gtk.Template.Child()
+
+    def __init__(self, window, **kwargs):
+        super().__init__(**kwargs)
+        self.set_transient_for(window)
+
+        # signals
+        self.row_console.connect('activated', self.__on_console_activated)
+        self.row_gparted.connect('activated', self.__on_gparted_activated)
+        self.row_handbook.connect('activated', self.__on_handbook_activated)
+        self.row_web.connect('activated', self.__on_web_activated)
+
+    def __on_console_activated(self, row):
+        GLib.spawn_command_line_async('kgx')
+
+    def __on_gparted_activated(self, row):
+        GLib.spawn_command_line_async('gparted')
+
+    def __on_handbook_activated(self, row):
+        webbrowser.open('https://handbook.vanillaos.org')
+
+    def __on_web_activated(self, row):
+        webbrowser.open('https://vanillaos.org')
