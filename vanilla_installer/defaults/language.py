d2f584bb110641d9ef30d4268e9d15f77ff599bd
Muqtadir <muqtxdir@gmail.com>
2023-08-28 13:53:11 +0530

    feat:[close #250] update the look of language/locale screen

--- vanilla_installer/defaults/language.py
+++ vanilla_installer/defaults/language.py
@@ -15,10 +15,31 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
-
 from gi.repository import Adw, Gtk
-
+from gi.repository import GnomeDesktop
 from vanilla_installer.core.languages import all_languages, current_language
+
+@Gtk.Template(resource_path="/org/vanillaos/Installer/gtk/widget-language.ui")
+class LanguageRow(Adw.ActionRow):
+    __gtype_name__ = "LanguageRow"
+
+    select_button = Gtk.Template.Child()
+    suffix_bin = Gtk.Template.Child()
+
+    def __init__(self, title, subtitle, selected_language, **kwargs):
+        super().__init__(**kwargs)
+        self.__title = title
+        self.__subtitle = subtitle
+        self.__selected_language = selected_language
+
+        self.set_title(title)
+        self.suffix_bin.set_label(subtitle)
+
+        self.select_button.connect("toggled", self.__on_check_button_toggled)
+
+    def __on_check_button_toggled(self, widget):
+        self.__selected_language["language_title"] = self.__title
+        self.__selected_language["language_subtitle"] = self.__subtitle
 
 
 @Gtk.Template(resource_path="/org/vanillaos/Installer/gtk/default-language.ui")
@@ -26,11 +47,11 @@ class VanillaDefaultLanguage(Adw.Bin):
     __gtype_name__ = "VanillaDefaultLanguage"
 
     btn_next = Gtk.Template.Child()
-    combo_languages = Gtk.Template.Child()
-    str_list_languages = Gtk.Template.Child()
     entry_search_language = Gtk.Template.Child()
-
     search_controller = Gtk.EventControllerKey.new()
+    all_languages_group = Gtk.Template.Child()
+
+    selected_language = {'language_title': None, 'language_subtitle': None}
 
     def __init__(self, window, distro_info, key, step, **kwargs):
         super().__init__(**kwargs)
@@ -39,40 +60,41 @@ class VanillaDefaultLanguage(Adw.Bin):
         self.__key = key
         self.__step = step
 
-        # set up the string list for all the languages
-        for _, lang in all_languages.items():
-            self.str_list_languages.append(lang)
-
-        # set up current language
-        for locale in all_languages.keys():
-            if current_language == locale:
-                self.combo_languages.set_selected(
-                    list(all_languages.keys()).index(locale)
-                )
-                break
+        self.__language_rows = self.__generate_language_list_widgets(self.selected_language)
+        for i, widget in enumerate(self.__language_rows):
+            self.all_languages_group.append(widget)
 
         # signals
         self.btn_next.connect("clicked", self.__window.next)
-        self.search_controller.connect(
-            "key-released", self.__on_search_key_pressed)
+        self.search_controller.connect("key-released", self.__on_search_key_pressed)
         self.entry_search_language.add_controller(self.search_controller)
+
+    def __generate_language_list_widgets(self, selected_language):
+        language_widgets = []
+        curr_language = GnomeDesktop.get_language_from_locale(current_language, None)
+
+        for language_subtitle, language_title in all_languages.items():
+            language_row = LanguageRow(language_title, language_subtitle, selected_language)
+            
+            if len(language_widgets)>0:
+                language_row.select_button.set_group(language_widgets[0].select_button)
+            language_widgets.append(language_row)
+
+            if curr_language == language_title:
+                language_row.select_button.set_active(True)
+
+        return language_widgets
 
     def get_finals(self):
         return {
-            "language": list(all_languages.keys())[self.combo_languages.get_selected()]
+            "language": self.selected_language["language_subtitle"]
         }
 
     def __on_search_key_pressed(self, *args):
-        keywords = self.entry_search_language.get_text().lower()
-        keywords = re.sub(r"[^a-zA-Z0-9 ]", "", keywords)
+        keywords = re.sub(r"[^a-zA-Z0-9 ]", "", self.entry_search_language.get_text().lower())
 
-        if keywords == "" or len(keywords) < 3:
-            return
-
-        for locale, lang in all_languages.items():
-            lang = re.sub(r"[^a-zA-Z0-9 ]", "", lang)
-            if re.search(keywords, lang, re.IGNORECASE):
-                self.combo_languages.set_selected(
-                    list(all_languages.keys()).index(locale)
-                )
-                break
+        for row in self.all_languages_group:
+            row_title = re.sub(r'[^a-zA-Z0-9 ]', '', row.get_title().lower())
+            row_subtitle = re.sub(r'[^a-zA-Z0-9 ]', '', row.suffix_bin.get_label().lower())
+            search_text = row_title + ' ' + row_subtitle
+            row.set_visible(re.search(keywords, search_text, re.IGNORECASE) is not None)
