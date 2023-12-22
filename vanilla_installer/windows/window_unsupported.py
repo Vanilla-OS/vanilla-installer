cfd458b7b2a46866ff3bab53eb818a324dacb46d
Muqtxdir <muqtxdir@gmail.com>
2023-12-23 03:05:25 +0530

    feat: #319
    - add unsupported window for non-uefi devices.

---
+++ vanilla_installer/windows/window_unsupported.py
@@ -0,0 +1,36 @@
+# window_unsupported.py
+#
+# Copyright 2023 muqtadir
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundationat version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gettext import gettext as _
+import subprocess
+from gi.repository import Adw, Gtk
+
+
+@Gtk.Template(resource_path="/org/vanillaos/Installer/gtk/window-unsupported.ui")
+class VanillaUnsupportedWindow(Adw.Window):
+    __gtype_name__ = "VanillaUnsupportedWindow"
+
+    btn_poweroff = Gtk.Template.Child()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.btn_poweroff.connect("clicked", self.__on_poweroff)
+
+    def __on_poweroff(self, btn):
+        subprocess.call(["systemctl", "poweroff"])
+
+
