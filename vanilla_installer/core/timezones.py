7b53100130b92d1dd20dd9d71d26924ad1490d50
Mateus Melchiades <matbme@duck.com>
2024-01-04 15:05:03 -0300

    Improve timezone names

--- vanilla_installer/core/timezones.py
+++ vanilla_installer/core/timezones.py
@@ -2,62 +2,53 @@ import datetime
 
 import requests
 from gi.repository import GnomeDesktop
+from gi.repository import GWeather
 from zoneinfo import ZoneInfo
 
-all_timezones = {}
+regions = {}
+world = GWeather.Location.get_world()
+parents = []
+base = world
+child = None
+while True:
+    child = base.next_child(child)
+    if child is not None:
+        if child.get_level() == GWeather.LocationLevel.REGION:
+            regions[child.get_name()] = {}
+            current_region = child.get_name()
+        elif child.get_level() == GWeather.LocationLevel.COUNTRY:
+            regions[current_region][child.get_name()] = {}
+            current_country = child.get_name()
+        elif child.get_level() == GWeather.LocationLevel.CITY:
+            regions[current_region][current_country][child.get_city_name()] = child.get_timezone_str()
 
-zone_tab_path = "/usr/share/zoneinfo/zone.tab"
+        if child.next_child(None) is not None:
+            parents.append(child)
+            base = child
+            child = None
+    else:
+        base = base.get_parent()
+        if base is None:
+            break
+        child = parents.pop()
 
-with open(zone_tab_path, "r") as zone_tab_file:
-    for line in zone_tab_file:
-        if line.startswith("#"):
-            continue
+all_timezones = dict(sorted(regions.items()))
 
-        fields = line.strip().split("\t")
-        if len(fields) < 3:
-            continue
-
-        country_code, city = fields[2].rsplit("/", 1)
-
-        if country_code not in all_timezones:
-            all_timezones[country_code] = []
-
-        all_timezones[country_code].append(city)
-
-all_timezones = {
-    country: sorted(timezones) for country, timezones in all_timezones.items()
-}
-all_timezones = dict(sorted(all_timezones.items()))
-
-
-def get_current_timezone():
-    success_code, country, city = get_timezone_by_ip()
-    if not success_code:
-        timezone = GnomeDesktop.WallClock().get_timezone()
-        timezone = timezone.get_identifier()
-
-        country = timezone.split("/")[0]
-        city = timezone[country.__len__() + 1 :]
-
-    return country, city
-
-
-def get_timezone_by_ip():
+def get_location():
     try:
-        res = requests.get("http://ip-api.com/json", timeout=3).json()
-        timezone = res["timezone"]
-        country = timezone.split("/")[0]
-        city = timezone[country.__len__() + 1 :]
-        success_code = True
-    except Exception:
-        success_code = False
-        city = -1
-        country = -1
-    return success_code, country, city
+        res = requests.get("http://ip-api.com/json?fields=49344", timeout=3).json()
+        if res["status"] != "success":
+            raise Exception(f"get_location: request failed with message '{res['message']}'")
+        nearest = world.find_nearest_city(res["lat"], res["lon"])
+    except Exception as e:
+        print(e)
+        nearest = None
+
+    return nearest
 
 
-def get_preview_timezone(country, city):
-    timezone = ZoneInfo(f"{country}/{city}")
+def get_timezone_preview(tzname):
+    timezone = ZoneInfo(tzname)
     now = datetime.datetime.now(timezone)
 
     return now.strftime("%H:%M"), now.strftime("%A, %d %B %Y")
