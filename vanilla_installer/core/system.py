c9d33e8e6fcbfb8a7741335208451a5d5ee1dcb3
Mateus Melchiades <matbme@duck.com>
2024-02-03 17:54:32 -0300

    Relax RAM requirement to 3.8 GB

--- vanilla_installer/core/system.py
+++ vanilla_installer/core/system.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 class Systeminfo:
     uefi = None
     ram = None
@@ -13,26 +14,32 @@ class Systeminfo:
             Systeminfo.uefi = proc.returncode == 0
 
         return Systeminfo.uefi
-    
+
     @staticmethod
     def is_ram_enough() -> bool:
         if not Systeminfo.ram:
-            proc = subprocess.Popen("free --giga | grep Mem | awk '{print $2}'",
-                                     shell=True, stdout=subprocess.PIPE).stdout\
-                                     .read().decode()
-            Systeminfo.ram = int(proc) >= 4
-        
+            proc = subprocess.Popen(
+                "free -b | grep Mem | awk '{print $2}'",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout.read().decode()
+            Systeminfo.ram = int(proc) >= 3800000000
+
         return Systeminfo.ram
-    
+
     @staticmethod
     def is_cpu_enough() -> bool:
         if not Systeminfo.cpu:
-            proc1 = subprocess.Popen("lscpu | egrep 'Core\(s\)' | awk '{print $4}'",
-                                     shell=True, stdout=subprocess.PIPE).stdout\
-                                     .read().decode()
-            proc2 = subprocess.Popen("lscpu | egrep 'Socket\(s\)' | awk '{print $2}'",
-                                     shell=True, stdout=subprocess.PIPE).stdout\
-                                     .read().decode()
+            proc1 = subprocess.Popen(
+                "lscpu | grep -E 'Core\\(s\\)' | awk '{print $4}'",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout.read().decode()
+            proc2 = subprocess.Popen(
+                "lscpu | grep -E 'Socket\\(s\\)' | awk '{print $2}'",
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout .read().decode()
             Systeminfo.cpu = (int(proc1) * int(proc2)) >= 2
-        
+
         return Systeminfo.cpu
